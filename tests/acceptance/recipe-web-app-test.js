747da73b3356b08f68002f91d41dc51f4e348ec6
mkulemeka <kahlekulemeka@gmail.com>
2021-11-07 16:22:02 -0500

    First commit

---
+++ tests/acceptance/recipe-web-app-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | recipe web app', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function (assert) {
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('h2').hasText('Welcome to M&J Recipes!');
+
+    assert.equal(currentURL(), '/about');
+
+  });
+});
